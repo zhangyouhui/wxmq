7b9d511c4f287783fca4ca684c8869f244bb08a9
zyh <1518742770@qq.com>
2019-12-03 11:16:31 +0800

    gg

--- weixin-java-mp-demo-springboot/src/main/java/com/example/demo/TestApplication.java
+++ weixin-java-mp-demo-springboot/src/main/java/com/example/demo/TestApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestApplication {
 
+	/***
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 	}
